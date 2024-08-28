0b1de1f478421a2a03b26aa7dc679bd5a90fd675
Ivan R. <41546276+kasper546@users.noreply.github.com>
2024-08-28 08:53:38 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p class="hours_minites">{{ texx }}</p>
+  <Calendar id="calendar-timeonly" v-model="time" timeOnly />
+  <Button icon="pi pi-calendar" @click="handleButtonClick"/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import Calendar from 'primevue/calendar';
+import Button from 'primevue/button';
+import 'primeicons/primeicons.css'; 
+import { ref } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const time = ref(null); // Время, выбранное пользователем
+const texx = ref(""); 
+
+// Обработчик клика на кнопку
+function handleButtonClick() {
+  if (time.value) {
+    const date = new Date(time.value); // Создаем новый объект Date
+    const hours = date.getHours(); // Получаем часы
+    const minutes = date.getMinutes(); // Получаем минуты
+    texx.value = `Часы: ${hours}, Минуты: ${minutes}`; // Выводим часы и минуты в консоль
+  } else {
+    console.log('Время не выбрано'); // Выводим, если время не выбрано
   }
 }
 </script>
