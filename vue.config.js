a714a49bc6cc6ebe792cdfaf9d2d59a2a6af218f
Ivan R. <41546276+kasper546@users.noreply.github.com>
2024-08-28 10:56:32 +0300

    fdsf

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/at-kons-project2/',
+  publicPath: '/at-kons-project2-1-/',
   transpileDependencies: true
 })
